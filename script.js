681fcd7a611663ea1caf4711b192579e6b5d290f
sandro cédric blatter <32453229+sandrobl@users.noreply.github.com>
2025-11-12 13:56:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+// Zeigt eine Messagebox an mit dem Text "hallo" drin. 
 alert("hallo");
 
+
+// Alle 2000ms wechsle die Hintergrundfarbe des Elements "Body" in eine zufällige R(Rot), G(Grün), B(Blau) Farbe. 
+// RGB Werte gehen von 0 - 255
 setInterval(() => {
     document.body.style.background = "rgb(" + Math.random() * 255 + ", " + Math.random() * 255 + ", " + Math.random() * 255 + ")";
 }, 2000);
